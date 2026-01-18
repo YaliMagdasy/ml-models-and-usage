6cdf6dec112962c8acfe6b488d2c7420fdbd44ea
Yali <yalimgds@gmail.com>
2026-01-18 02:22:35 +0200

    Added weighted option Added the option to use a weighted prediction.

--- src/knn_classifier.py
+++ src/knn_classifier.py
@@ -7,16 +7,29 @@ class KNNClassifier:
         self.y_train = y
         
     # Predicting
-    def predict(self, X, k: int=5):
+    def predict(self, X, k: int=5, weighted: bool=False):
         predictions = []
         for x_pred in X:
             distances = np.linalg.norm(self.X_train - x_pred, axis=1)
-            
             k_indices = np.argpartition(distances, k)[:k]
             
             k_nearest_labels = self.y_train[k_indices]
             
-            most_common = np.bincount(k_nearest_labels).argmax()
-            predictions.append(most_common)
-            
+            if weighted:
+                k_nearest_dists = distances[k_indices]
+                weights = 1 / (k_nearest_dists + 1e-10)
+                
+                classes = np.unique(k_nearest_labels)
+                class_scores = {}
+                
+                for c in classes:
+                    is_class_c = (k_nearest_labels == c)
+                    class_scores[c] = np.sum(weights[is_class_c])
+
+                predictions.append(max(class_scores, key=class_scores.get))
+            else:
+                counts = np.bincount(k_nearest_labels.astype(int))
+
+                predictions.append(np.argmax(counts))
+                
         return np.array(predictions)
--- src/knn_regressor.py
+++ src/knn_regressor.py
@@ -7,7 +7,7 @@ class KNNRegressor:
         self.y_train = y
         
     # Predicting
-    def predict(self, X, k: int=5):
+    def predict(self, X, k: int=5, weighted: bool=False):
         predictions = []
         for x_pred in X:
             distances = np.linalg.norm(self.X_train - x_pred, axis=1)
@@ -15,8 +15,18 @@ class KNNRegressor:
             k_indices = np.argpartition(distances, k)[:k]
             
             k_nearest_labels = self.y_train[k_indices]
-            
-            average = np.mean(k_nearest_labels)
-            predictions.append(average)
+
+            if weighted:
+                k_nearest_dists = distances[k_indices]
+                weights = 1 / (k_nearest_dists + 1e-10)
+
+                weighted_average = np.sum(k_nearest_labels * weights) / np.sum(weights)
+                
+                predictions.append(weighted_average)
+    
+            else:
+                average = np.mean(k_nearest_labels)
+
+                predictions.append(average)
             
         return np.array(predictions)
