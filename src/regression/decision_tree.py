c2cd0020db63d61a18bd697e9e9fb7970686cfc7
Yali <yalimgds@gmail.com>
2026-02-13 20:44:49 +0200

    Implemented Random Forest Regressor - Adjusted the Decision Tree Regressor to support Random Forest, and fixed some bugs in the process. (same tweaks as for the classifier) - Implemented the Random Forest Regressor.

--- src/regression/decision_tree.py
+++ src/regression/decision_tree.py
@@ -19,19 +19,26 @@ class DecisionTreeRegressor:
         self.root = None
 
     def fit(self, X, y):
-        self.n_features = X.shape[1] if not self.n_features else min(X.shape[1], self.n_features)
+        n_available_features = X.shape[1]
+        if self.n_features is None:
+            self.n_features = n_available_features
+        else:
+            self.n_features = min(n_available_features, self.n_features)
+            
         self.root = self.__build_tree(X, y)
 
     def __build_tree(self, X, y, depth=0):
         n_samples, n_feats = X.shape
-        n_labels = len(np.unique(y))
-
-        if (depth >= self.max_depth or n_labels == 1 or n_samples < self.min_samples_split):
+        
+        if (depth >= self.max_depth or n_samples < self.min_samples_split or np.var(y) < 1e-7):
             leaf_value = self.__calculate_leaf_value(y)
             return _Node(value=leaf_value)
 
         feat_idxs = np.random.choice(n_feats, self.n_features, replace=False)
         best_feat, best_thresh = self.__best_split(X, y, feat_idxs)
+
+        if best_feat is None:
+            return _Node(value=self.__calculate_leaf_value(y))
 
         left_idxs, right_idxs = self.__split(X[:, best_feat], best_thresh)
         left = self.__build_tree(X[left_idxs, :], y[left_idxs], depth + 1)
@@ -90,33 +97,32 @@ class DecisionTreeRegressor:
             return self.__traverse_tree(x, node.left)
         return self.__traverse_tree(x, node.right)
     
-
     def print_tree(self, figsize=(12, 8)):
-            import matplotlib.pyplot as plt
+        import matplotlib.pyplot as plt
 
-            def get_depth(node):
-                if not node: return 0
-                if node.is_leaf_node(): return 1
-                return max(get_depth(node.left), get_depth(node.right)) + 1
+        def get_depth(node):
+            if not node: return 0
+            if node.is_leaf_node(): return 1
+            return max(get_depth(node.left), get_depth(node.right)) + 1
 
-            def get_width(node):
-                if not node: return 0
-                if node.is_leaf_node(): return 1
-                return get_width(node.left) + get_width(node.right)
+        def get_width(node):
+            if not node: return 0
+            if node.is_leaf_node(): return 1
+            return get_width(node.left) + get_width(node.right)
 
-            depth = get_depth(self.root)
-            width = get_width(self.root)
+        depth = get_depth(self.root)
+        width = get_width(self.root)
 
-            dynamic_width = max(figsize[0], width * 0.8)
-            dynamic_height = max(figsize[1], depth * 1.0)
+        dynamic_width = max(figsize[0], width * 0.8)
+        dynamic_height = max(figsize[1], depth * 1.0)
 
-            fig, ax = plt.subplots(figsize=(dynamic_width, dynamic_height))
-            ax.axis("off")
-            
-            self.leaf_count = 0
-            self._plot_node(self.root, depth, 0, ax)
-            plt.tight_layout()
-            plt.show()
+        fig, ax = plt.subplots(figsize=(dynamic_width, dynamic_height))
+        ax.axis("off")
+        
+        self.leaf_count = 0
+        self._plot_node(self.root, depth, 0, ax)
+        plt.tight_layout()
+        plt.show()
 
     def _plot_node(self, node, total_depth, current_depth, ax):
         if node is None:
